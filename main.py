b32fd40968534d71cd50d7c0d441d4bb8a40919b
Michael Usachenko <musachenko@gmail.com>
2017-01-02 01:05:06 -0600

    commit for newspaper code
    started basic summary extraction from marketwatch articles

---
+++ main.py
@@ -0,0 +1,24 @@
+import newspaper 
+from newspaper import news_pool
+import markovify
+
+marketwatch = newspaper.build('http://www.marketwatch.com')
+
+# bloomberg = newspaper.build('https://www.bloomberg.com')
+# cnbc = newspaper.build('http://www.cnbc.com')
+# gfin = newspaper.build('https://www.google.com/finance')
+
+# news = [marketwatch, bloomberg, cnbc, gfin]
+# news_pool.set(papers, threads_per_source=2) # (3*2) = 6 threads total
+# news_pool.join()
+
+for article in marketwatch.articles: # gets recent articles && checks for dupes
+	print (article.url)
+	marketwatch.size()
+
+mainarticle = marketwatch.articles[0] # referencing market watch as source
+mainarticle.download() # gets most recent article
+mainarticle.parse() # parses text from article
+mainarticle.nlp() # preps for extracting summary
+print (mainarticle.summary)
+
